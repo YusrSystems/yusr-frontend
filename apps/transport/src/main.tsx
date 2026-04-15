4ef8db6c147163a2db6873e5413744f745fd130b
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-04-15 14:46:34 +0300

    connect- transport project with packages

--- apps/transport/src/main.tsx
+++ apps/transport/src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { ApiConstants } from "@yusr_systems/core";
+ApiConstants.initialize("https://yusrbus.runasp.net/api");
+// ApiConstants.initialize("https://localhost:7226/api");
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
