6057c5916f7b1a4c28dc689d517b9be004c74d8d
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-04-15 14:50:42 +0300

    init- redux & react redux

--- apps/transport/src/main.tsx
+++ apps/transport/src/main.tsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ApiConstants } from "@yusr_systems/core";
+import { store } from './core/store.ts';
+import { Provider } from "react-redux";
+
+
+
+
 ApiConstants.initialize("https://yusrbus.runasp.net/api");
 // ApiConstants.initialize("https://localhost:7226/api");
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
