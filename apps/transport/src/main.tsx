a4f6f384690936d37ae57c45779e7e1a947acc04
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-04-19 23:51:23 +0300

    feat: yusr app, making main filer simpler

--- apps/transport/src/main.tsx
+++ apps/transport/src/main.tsx
@@ -1,22 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./app/app.tsx";
-import ErrorBoundary from "./app/features/error/errorBoundary.tsx";
 import "./index.css";
-import { Provider } from "react-redux";
 import { store } from "./app/core/state/store.ts";
-import { ApiConstants } from "yusr-core";
-
-ApiConstants.initialize("https://yusrbus.runasp.net/api");
-// ApiConstants.initialize("https://localhost:7226/api");
-
+import { YusrApp } from "yusr-ui";
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </Provider>
-  </StrictMode>
+
+
+  <YusrApp store={store} backendUrl="https://yusrbus.runasp.net/api" >
+    <App />
+  </YusrApp>
 );
