de4204f600b2dd7beae5fb59738da0d8703d7a6a
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-04-20 17:46:15 +0300

    enhance: allowing yusr-ui to be self-contained for tailwind and shadcn config

--- apps/transport/src/main.tsx
+++ apps/transport/src/main.tsx
@@ -1,12 +1,9 @@
 import { createRoot } from "react-dom/client";
-
 import App from "./app/app.tsx";
-import "./index.css";
 import { store } from "./app/core/state/store.ts";
 import { YusrApp } from "yusr-ui";
+
 createRoot(document.getElementById("root")!).render(
-
-
   <YusrApp store={store} backendUrl="https://yusrbus.runasp.net/api" >
     <App />
   </YusrApp>
