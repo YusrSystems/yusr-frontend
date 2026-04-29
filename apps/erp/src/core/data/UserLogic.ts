acdf0c434bc68d1ccc05d00c86251fb5aaca1b3a
3braa04 <braabormo@gmail.com>
2026-04-29 23:15:08 +0300

    fix UserSlice

--- apps/erp/src/core/data/UserLogic.ts
+++ apps/erp/src/core/data/UserLogic.ts
@@ -21,15 +21,15 @@ export class UserValidationRules
 
 export class UserSlice
 {
-  private static entitySliceInstance = createGenericEntitySlice("branch", new UsersApiService());
+  private static entitySliceInstance = createGenericEntitySlice("user", new UsersApiService());
   public static entityActions = UserSlice.entitySliceInstance.actions;
   public static entityReducer = UserSlice.entitySliceInstance.reducer;
 
-  private static dialogSliceInstance = createGenericDialogSlice<User>("branchDialog");
+  private static dialogSliceInstance = createGenericDialogSlice<User>("userDialog");
   public static dialogActions = UserSlice.dialogSliceInstance.actions;
   public static dialogReducer = UserSlice.dialogSliceInstance.reducer;
 
-  private static formSliceInstance = createGenericFormSlice<User>("branchForm");
+  private static formSliceInstance = createGenericFormSlice<User>("userForm");
   public static formActions = UserSlice.formSliceInstance.actions;
   public static formReducer = UserSlice.formSliceInstance.reducer;
 }
