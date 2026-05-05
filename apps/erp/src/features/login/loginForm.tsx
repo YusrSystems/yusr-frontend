71f5a504172de0b084b0f7dd1c6b1280499ca41d
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-05-05 22:27:18 +0300

    chore: remove un used import

--- apps/erp/src/features/login/loginForm.tsx
+++ apps/erp/src/features/login/loginForm.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { ApiConstants, Button, Card, CardContent, Checkbox, cn, Field, FieldDescription, FieldGroup, LoginRequest, PasswordField, TextField, useEntityForm, User, type ValidationRule, Validators, YusrApiHelper } from "yusr-ui";
 import type { Setting } from "../../core/data/setting";
-import { login, updateLoggedInUser, useAppDispatch, useAppSelector } from "../../core/state/store";
+import { login, updateLoggedInUser, useAppDispatch } from "../../core/state/store";
 
 export function LoginForm({ className, ...props }: React.ComponentProps<"div">)
 {
