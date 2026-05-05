ef8d3f13441c86307015550e3f6e2c902c9c891d
3braa04 <braabormo@gmail.com>
2026-05-05 20:24:49 +0300

    fix: transport failure

--- apps/transport/src/main.tsx
+++ apps/transport/src/main.tsx
@@ -1,16 +1,13 @@
-import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import { LoadingTranslations, YusrApp } from "yusr-ui";
+import { YusrApp } from "yusr-ui";
 import App from "./app/app.tsx";
 import { store } from "./app/core/state/store.ts";
 import "./config/i18n.ts";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <Suspense fallback={ <LoadingTranslations /> }>
-    <YusrApp store={ store } backendUrl="https://yusrbus.runasp.net/api" // backendUrl="https://localhost:7142/api"
-    >
-      <App />
-    </YusrApp>
-  </Suspense>
+  <YusrApp store={ store } backendUrl="https://yusrbus.runasp.net/api" // backendUrl="https://localhost:7142/api"
+  >
+    <App />
+  </YusrApp>
 );
