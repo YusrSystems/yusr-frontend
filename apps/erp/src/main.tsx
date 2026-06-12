19d5e79ecd7dd786ebaee982d8afbafb0a469c23
3braa04 <braabormo@gmail.com>
2026-06-12 06:44:22 +0300

    feat: add vercel SpeedInsights

--- apps/erp/src/main.tsx
+++ apps/erp/src/main.tsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import { i18n, YusrApp } from "yusr-ui";
 import App from "./app/app.tsx";
 import { store } from "./core/state/store.ts";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
 function revealApp()
@@ -36,5 +37,6 @@ createRoot(document.getElementById("root")!).render(
   >
     <App />
     <Analytics />
+    <SpeedInsights />
   </YusrApp>
 );
