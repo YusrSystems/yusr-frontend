55bb670843efda0b6da3db4693f0f6230d2df8bf
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-06-16 17:59:31 +0300

    refactor: remove slice and redux staff

--- packages/yusr-ui/src/state/authListener.ts
+++ packages/yusr-ui/src/state/authListener.ts
@@ -1,37 +1,21 @@
-import type { Dispatch, UnknownAction } from "@reduxjs/toolkit";
 import { AuthConstants } from "../auth";
+
 
 interface AuthActions
 {
-  logout: () => any;
-  syncFromStorage: () => any;
+	logout: () => any;
+	syncFromStorage: () => any;
 }
-
-export const setupAuthListenersForRedux = (dispatch: Dispatch<UnknownAction>, actions: AuthActions) =>
-{
-  window.addEventListener("storage", (e) =>
-  {
-    if (e.key === AuthConstants.AuthCheckStorageItemName)
-    {
-      dispatch(actions.syncFromStorage());
-    }
-  });
-
-  window.addEventListener(AuthConstants.UnauthorizedEventName, () =>
-  {
-    dispatch(actions.logout());
-  });
-};
 
 export const setupAuthListeners = (actions: AuthActions) =>
 {
-  window.addEventListener(AuthConstants.AuthCheckStorageItemName, () =>
-  {
-    actions.syncFromStorage();
-  });
+	window.addEventListener(AuthConstants.AuthCheckStorageItemName, () =>
+	{
+		actions.syncFromStorage();
+	});
 
-  window.addEventListener(AuthConstants.UnauthorizedEventName, () =>
-  {
-    actions.logout();
-  });
+	window.addEventListener(AuthConstants.UnauthorizedEventName, () =>
+	{
+		actions.logout();
+	});
 };
