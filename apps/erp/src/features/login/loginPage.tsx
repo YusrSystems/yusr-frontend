e91608d7c832d99cc35a6c95ffd553b944661f2a
3braa04 <braabormo@gmail.com>
2026-06-20 14:39:00 +0300

    chore: logout user if he in login/register pages, and somehow he still has the jwt cookie
    this solves the issue when you try to log in, and you got an error "username or password is incorrect"

--- apps/erp/src/features/login/loginPage.tsx
+++ apps/erp/src/features/login/loginPage.tsx
@@ -1,14 +1,28 @@
-import { YusrBackground } from "yusr-ui";
+import { YusrApiHelper, YusrBackground } from "yusr-ui";
 import { LoginForm } from "./loginForm";
+import { Services } from "@/core/services/services.ts";
+
 
 export default function LoginPage()
 {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <YusrBackground />
-      <div className="w-full max-w-sm md:max-w-4xl">
-        <LoginForm />
-      </div>
-    </div>
-  );
+	const Logout = async () =>
+	{
+		const result = await YusrApiHelper.Post(`/api/Logout`);
+
+		if (result.status === 200 || result.status === 204)
+		{
+			Services.auth.logout();
+		}
+	};
+
+	void Logout();
+
+	return (
+		<div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+			<YusrBackground/>
+			<div className="w-full max-w-sm md:max-w-4xl">
+				<LoginForm/>
+			</div>
+		</div>
+	);
 }
