352ea1ca4eaf15d5bd8484efe6b06dfbd5ed97ba
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-06-23 12:56:14 +0300

    chore: - re-format crud table header

--- packages/yusr-ui/src/components/custom/table/crudTableHeader.tsx
+++ packages/yusr-ui/src/components/custom/table/crudTableHeader.tsx
@@ -2,39 +2,42 @@ import { PlusIcon } from "lucide-react";
 import { type ReactNode } from "react";
 import { Button } from "../../pure/button";
 
+
 export type CrudTableHeaderProps = {
-  title: string;
-  addButtonTitle: string;
-  isAddButtonVisible?: boolean;
-  onAddButtonClicked: () => void;
-  actionButtons?: ReactNode[];
+	title: string;
+	addButtonTitle: string;
+	isAddButtonVisible?: boolean;
+	onAddButtonClicked: () => void;
+	actionButtons?: ReactNode[];
 };
+
 export function CrudTableHeader(
-  {
-    title,
-    addButtonTitle,
-    isAddButtonVisible = true,
-    onAddButtonClicked,
-    actionButtons
-  }: CrudTableHeaderProps
+	{
+		title,
+		addButtonTitle,
+		isAddButtonVisible = true,
+		onAddButtonClicked,
+		actionButtons
+	}: CrudTableHeaderProps
 )
 {
-  return (
-    <>
-      <div className="flex justify-between mb-8 gap-3">
-        <div>
-          <h1>{ title }</h1>
-        </div>
-        <div className="flex gap-3">
-          { ...actionButtons ?? [] }
-          { isAddButtonVisible && (
-            <Button variant="default" onClick={ onAddButtonClicked }>
-              <PlusIcon className="h-4 w-4" />
-              { addButtonTitle }
-            </Button>
-          ) }
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="flex justify-between mb-8 gap-3">
+				<div>
+					<h1>{ title }</h1>
+				</div>
+				<div className="flex gap-3">
+					{ ...actionButtons ?? [] }
+
+					{ isAddButtonVisible && (
+						<Button variant="default" onClick={ onAddButtonClicked }>
+							<PlusIcon className="h-4 w-4"/>
+							{ addButtonTitle }
+						</Button>
+					) }
+				</div>
+			</div>
+		</>
+	);
 }
