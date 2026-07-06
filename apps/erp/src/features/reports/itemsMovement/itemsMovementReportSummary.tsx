e45a65c5547c061a2b0341d6e6b05bfd82b54529
3braa04 <braabormo@gmail.com>
2026-07-06 15:54:38 +0300

    chore: fix item movement summary

--- apps/erp/src/features/reports/itemsMovement/itemsMovementReportSummary.tsx
+++ apps/erp/src/features/reports/itemsMovement/itemsMovementReportSummary.tsx
@@ -1,12 +1,12 @@
 import { SummaryRow } from "@/features/report/components/summaryRow.tsx";
 import { formatNumber } from "@/features/report/utils/formating.ts";
-import { ItemsMovementReportType } from "@/features/reports/itemsMovement/itemsMovementReportResult.ts";
 import { Cubits } from "@/core/services/cubits.ts";
+import { ItemsMovementReportType } from "@/features/reports/itemsMovement/itemsMovementReportResult.ts";
 
 
 export function ItemsMovementReportSummary()
 {
-	if (Cubits.ItemsMovementReport.result.value?.reportType !== ItemsMovementReportType.ItemsMovement)
+	if (Cubits.ItemsMovementReport.result.value == undefined)
 	{
 		return null;
 	}
@@ -17,48 +17,57 @@ export function ItemsMovementReportSummary()
 	return (
 		<div
 			className="max-w-md my-3 border border-border rounded-lg overflow-hidden ms-auto divide-y divide-border">
-			<SummaryRow>
-				<div>
-					<SummaryRow.Label label="مجموع الكمية الواردة"/>
-					<SummaryRow.Label label="Total incoming qtn"/>
-				</div>
-				<SummaryRow.Value value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalIncomeQtn) }/>
-			</SummaryRow>
-			<SummaryRow>
-				<div>
-					<SummaryRow.Label label="مجموع الكمية الصادرة"/>
-					<SummaryRow.Label label="Total exported qtn"/>
-				</div>
-				<SummaryRow.Value value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalOutcomeQtn) }/>
-			</SummaryRow>
-			<SummaryRow>
-				<div>
-					<SummaryRow.Label label="صافي الكمية"/>
-					<SummaryRow.Label label="Net qtn"/>
-				</div>
-				<SummaryRow.Value value={ formatNumber(netQtn) }/>
-			</SummaryRow>
-			<SummaryRow>
-				<div>
-					<SummaryRow.Label label="مجموع المبلغ الوارد"/>
-					<SummaryRow.Label label="Total incoming amount"/>
-				</div>
-				<SummaryRow.Value value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalIncomeAmount) }/>
-			</SummaryRow>
-			<SummaryRow>
-				<div>
-					<SummaryRow.Label label="مجموع المبلغ الصادر"/>
-					<SummaryRow.Label label="Total outcoming amount"/>
-				</div>
-				<SummaryRow.Value value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalOutcomeAmount) }/>
-			</SummaryRow>
-			<SummaryRow>
-				<div>
-					<SummaryRow.Label label="صافي المبلغ"/>
-					<SummaryRow.Label label="Net amount"/>
-				</div>
-				<SummaryRow.Value value={ formatNumber(netAmount) }/>
-			</SummaryRow>
+			{ Cubits.ItemsMovementReport.result.value.reportType === ItemsMovementReportType.ItemsMovement && (
+
+				<>
+					<SummaryRow>
+						<div>
+							<SummaryRow.Label label="مجموع الكمية الواردة"/>
+							<SummaryRow.Label label="Total incoming qtn"/>
+						</div>
+						<SummaryRow.Value
+							value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalIncomeQtn) }/>
+					</SummaryRow>
+					<SummaryRow>
+						<div>
+							<SummaryRow.Label label="مجموع الكمية الصادرة"/>
+							<SummaryRow.Label label="Total exported qtn"/>
+						</div>
+						<SummaryRow.Value
+							value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalOutcomeQtn) }/>
+					</SummaryRow>
+					<SummaryRow>
+						<div>
+							<SummaryRow.Label label="صافي الكمية"/>
+							<SummaryRow.Label label="Net qtn"/>
+						</div>
+						<SummaryRow.Value value={ formatNumber(netQtn) }/>
+					</SummaryRow>
+					<SummaryRow>
+						<div>
+							<SummaryRow.Label label="مجموع المبلغ الوارد"/>
+							<SummaryRow.Label label="Total incoming amount"/>
+						</div>
+						<SummaryRow.Value
+							value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalIncomeAmount) }/>
+					</SummaryRow>
+					<SummaryRow>
+						<div>
+							<SummaryRow.Label label="مجموع المبلغ الصادر"/>
+							<SummaryRow.Label label="Total outcoming amount"/>
+						</div>
+						<SummaryRow.Value
+							value={ formatNumber(Cubits.ItemsMovementReport.result.value.totalOutcomeAmount) }/>
+					</SummaryRow>
+					<SummaryRow>
+						<div>
+							<SummaryRow.Label label="صافي المبلغ"/>
+							<SummaryRow.Label label="Net amount"/>
+						</div>
+						<SummaryRow.Value value={ formatNumber(netAmount) }/>
+					</SummaryRow>
+				</>
+			) }
 			<SummaryRow className="bg-muted/50">
 				<div>
 					<SummaryRow.Label label="مجموع الربح"/>
